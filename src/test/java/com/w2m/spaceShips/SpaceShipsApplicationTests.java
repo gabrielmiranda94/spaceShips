dc980b0f1788d4a16993d795fa14604848b60317
gabrielmiranda94 <gabriel.miranda94@hotmail.com>
2024-10-27 13:58:59 +0100

    Initial commit

---
+++ src/test/java/com/w2m/spaceShips/SpaceShipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w2m.spaceShips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceShipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
