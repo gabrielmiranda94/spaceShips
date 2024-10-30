a20a9b4cde72ac417a326420a50b3c5a710f0861
gabrielmiranda94 <gabriel.miranda94@hotmail.com>
2024-10-30 20:40:35 +0100

    docker compose en producer

---
+++ src/main/java/com/w2m/spaceShips/infrastructure/messaging/SpaceshipProducer.java
@@ -0,0 +1,21 @@
+package com.w2m.spaceShips.infrastructure.messaging;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpaceshipProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public SpaceshipProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("spaceshipTopic", message);
+    }
+}
