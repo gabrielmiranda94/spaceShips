5bbdf7d4438ebaa97bb29a2455f20122cfc0ffdb
gabrielmiranda94 <gabriel.miranda94@hotmail.com>
2024-10-31 20:06:36 +0100

    integ test

--- src/test/java/com/w2m/spaceShips/api/SpaceshipControllerIntegrationTest.java
+++ src/test/java/com/w2m/spaceShips/api/SpaceshipControllerIntegrationTest.java
@@ -1,12 +1,17 @@
 package com.w2m.spaceShips.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.w2m.spaceShips.infrastructure.dto.AuthRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,10 +22,21 @@ public class SpaceshipControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
+
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
     @Test
-    public void shouldReturnSpaceshipList() throws Exception {
-        mockMvc.perform(get("/spaceships"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isArray());
+    void testLogin() throws Exception {
+        AuthRequest authRequest = AuthRequest.builder().username("user").password("password").build();
+
+        mockMvc.perform(post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(authRequest)))
+                .andExpect(status().isOk());
     }
 }
